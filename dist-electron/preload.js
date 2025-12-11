e21d281c82d718d8dd9d7c9371cebc121cb01703
slammers001 <slammers@einhorn.co>
2025-12-10 22:05:35 -0500

    updated

---
+++ dist-electron/preload.js
@@ -0,0 +1,23 @@
+"use strict";
+const { contextBridge, ipcRenderer } = require("electron");
+const electronAPI = {
+  send: (channel, data) => {
+    const validChannels = ["toMain"];
+    if (validChannels.includes(channel)) {
+      ipcRenderer.send(channel, data);
+    }
+  },
+  receive: (channel, func) => {
+    const validChannels = ["fromMain"];
+    if (validChannels.includes(channel)) {
+      const subscription = (_event, ...args) => func(...args);
+      ipcRenderer.on(channel, subscription);
+      return () => {
+        ipcRenderer.removeListener(channel, subscription);
+      };
+    }
+    return () => {
+    };
+  }
+};
+contextBridge.exposeInMainWorld("electron", electronAPI);
